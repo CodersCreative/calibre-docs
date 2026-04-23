5418b9c23296a9ece1cbb7cd1e10ea57b3594798
Creative Coders <113299264+CodersCreative@users.noreply.github.com>
2026-04-24 00:35:36 +0200

    Added calibre branding

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,51 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import catppuccin from "@catppuccin/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      plugins: [
+        catppuccin({
+          dark: { flavor: "mocha", accent: "blue" },
+          light: { flavor: "latte", accent: "blue" },
+        }),
+      ],
+      title: "Calibre",
+      customCss: [
+        "@fontsource/poppins/400.css",
+        "@fontsource/poppins/600.css",
+        "./src/styles/custom.css",
+      ],
+      logo: {
+        light: "./src/assets/calibre.svg",
+        dark: "./src/assets/calibrelight.svg",
+        replacesTitle: true,
+      },
+      social: [
+        {
+          icon: "youtube",
+          label: "YouTube",
+          href: "https://www.youtube.com/@crvcode",
+        },
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/CodersCreative/calibre-lang",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Language Tour",
+          autogenerate: { directory: "tour" },
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
